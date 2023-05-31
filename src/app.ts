8ac99a8869f246a0f5a8a9da88e8e2091d992e86
Sakib Rahman <seu.sakib@gmail.com>
2023-05-31 16:11:55 +0600

    [done] created .env

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express, { Application, Response } from "express";
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_req, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
